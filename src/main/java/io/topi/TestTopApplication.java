04c09ebd7938177a8da0b9f37dac49102c43ca9e
robson nunes <rob_sons@hotmail.com>
2020-11-16 20:51:45 -0300

    Processo Seletivo TOPi - Teste Técnico

---
+++ src/main/java/io/topi/TestTopApplication.java
@@ -0,0 +1,13 @@
+package io.topi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTopApplication.class, args);
+	}
+
+}
